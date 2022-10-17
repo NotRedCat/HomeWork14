f85d6d5eb424b63d354ae1d484ef5dc7f511d416
noredcat <anastasiaredcat@gmail.com>
2022-10-17 21:53:04 +0700

    first commit

---
+++ src/test/java/com/demoqa/tests/RegistrationFormTests.java
@@ -0,0 +1,64 @@
+package com.demoqa.tests;
+
+import com.codeborne.selenide.Configuration;
+import com.demoqa.pages.RegistrationFormPage;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static com.demoqa.tests.TestData.*;
+import static io.qameta.allure.Allure.step;
+import static java.lang.String.format;
+
+public class RegistrationFormTests extends TestBase {
+    RegistrationFormPage registrationFormPage = new RegistrationFormPage();
+
+    public void setRegistrationFormPage(RegistrationFormPage registrationFormPage) {
+
+    }
+
+    @Test
+    void fillFormTest() {
+        step("Open registration form", () ->
+        {
+            registrationFormPage.openPage();
+        });
+        step("Filling out  the form with data", () ->
+        {
+            registrationFormPage.setFirstName(firstName)
+                    .setLastName(lastName)
+                    .setEmail(email)
+                    .setGender(gender)
+                    .setUserNumber(number)
+                    .setBirtDate(day, month, year)
+                    .setSubject(subject)
+                    .setHobby(hobby)
+                    .uploadFile(file)
+                    .setAddress(address)
+                    .setValuesStateAndCity(state, city);
+        });
+        step("Submitting a form", () ->
+        {
+            registrationFormPage.clickSubmit();
+        });
+        step("Checking for modal visibility", () ->
+        {
+            registrationFormPage.checkResultsModalVisible();
+        });
+        step("Checking that the data is correct", () ->
+        {
+            registrationFormPage.checkResult("Student Name", expectedFullName)
+                    .checkResult("Student Email", email)
+                    .checkResult("Gender", gender)
+                    .checkResult("Mobile", number)
+                    .checkResult("Date of Birth", format(("%s %s,%s"), day, month, year))
+                    .checkResult("Subjects", subject)
+                    .checkResult("Hobbies", hobby)
+                    .checkResult("Picture", file)
+                    .checkResult("Address", address)
+                    .checkResult("State and City", state + " " + city);
+        });
+
+    }
+}
---
+++ src/test/java/com/demoqa/utils/RandomUtils.java
@@ -0,0 +1,10 @@
+package com.demoqa.utils;
+
+import com.github.javafaker.service.RandomService;
+
+import java.security.SecureRandom;
+import java.util.*;
+
+public class RandomUtils {
+
+}
